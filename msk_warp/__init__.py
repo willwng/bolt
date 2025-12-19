27ab85c1e90a096c4befe53d5bccdf18866af111
William Wang <wyw1@stanford.edu>
2025-12-18 16:04:56 -0800

    expose fk

--- msk_warp/__init__.py
+++ msk_warp/__init__.py
@@ -2,6 +2,7 @@
 
 from ._src.step import step_to as step_to
 from ._src.forward import reset as reset
+from ._src.forward import fk as fk
 from ._src.types import Model as Model
 from ._src.types import Data as Data
 from ._src.types import Option as Option
