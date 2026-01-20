af7d99bfa9a0e1a78d25b7ae88a6812875f1db34
William Wang <wyw1@stanford.edu>
2026-01-20 12:23:41 -0800

    user-facing hunt crossley limit forces

--- msk_warp/_src/smooth_hc_limits.py
+++ msk_warp/_src/smooth_hc_limits.py
@@ -13,6 +13,7 @@ def _process_joint_limits(
         limit_dof_adr: wp.array(dtype=int),
         limit_dof_qadr: wp.array(dtype=int),
         limit_dof_forces: wp.array(dtype=wp.vec2),
+        limit_dof_shapes: wp.array(dtype=wp.vec2),
         # Data in:
         qpos_in: wp.array2d(dtype=float),
         qvel_in: wp.array2d(dtype=float),
@@ -26,18 +27,16 @@ def _process_joint_limits(
     dof_adr = limit_dof_adr[limitdofid]
     dof_qadr = limit_dof_qadr[limitdofid]
     limit_forces = limit_dof_forces[limitdofid]
+    shape_factors = limit_dof_shapes[limitdofid]
     qpos = qpos_in[worldid, dof_qadr]
     qvel = qvel_in[worldid, dof_adr]
 
-    # todo: don't hardcode these
-    damping = 1.0
-    stiffness = 500.0
     if qpos >= dof_range[0] and qpos <= dof_range[1]:
         return
     elif qpos > dof_range[1]:
-        force = wp.min(-stiffness * (qpos - dof_range[1]) * (1.0 + damping * qvel), 0.0)
+        force = wp.min(-limit_forces[1] * (qpos - dof_range[1]) * (1.0 + shape_factors[1] * qvel), 0.0)
     else:
-        force = wp.max(-stiffness * (qpos - dof_range[0]) * (1.0 - damping * qvel), 0.0)
+        force = wp.max(-limit_forces[0] * (qpos - dof_range[0]) * (1.0 - shape_factors[0] * qvel), 0.0)
 
     wp.atomic_add(qfrc_applied_out, worldid, dof_adr, force)
     wp.atomic_add(dof_limit_torque_out, worldid, limitdofid, force)
@@ -53,6 +52,7 @@ def apply_limit_forces(m: Model, d: Data):
             m.limit_dof_adr,
             m.limit_dof_qadr,
             m.limit_dof_forces,
+            m.limit_dof_shapes,
             d.qpos,
             d.qvel
         ],
