501ae34df11044e0f32b61bd14fd75185cd1b0cf
William Wang <wyw1@stanford.edu>
2026-04-07 12:31:44 -0700

    just add normal force for contacts

--- msk_warp/_src/smooth_hunt_crossley.py
+++ msk_warp/_src/smooth_hunt_crossley.py
@@ -110,8 +110,8 @@ def _process_contacts_hc(
 
     # Same body-group contact (TODO: fix me for if there is ever more than one model)
     if body1 != 0 and body2 != 0:
-        wp.atomic_add(geom_self_cforce_out[worldid], geom[0], wp.length(force))
-        wp.atomic_add(geom_self_cforce_out[worldid], geom[1], wp.length(force))
+        wp.atomic_add(geom_self_cforce_out[worldid], geom[0], wp.abs(f))
+        wp.atomic_add(geom_self_cforce_out[worldid], geom[1], wp.abs(f))
 
     # Keep track of ground reaction forces
     if body1 == 0:
